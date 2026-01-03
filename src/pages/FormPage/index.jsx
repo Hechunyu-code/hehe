c61852e763147a60a1906d5c4fd36db56b7175b7
he chunyu <3448694693@qq.com>
2026-01-03 16:48:02 +0800

    feat:手机号输入框加绿色边框

--- src/pages/FormPage/index.jsx
+++ src/pages/FormPage/index.jsx
@@ -106,7 +106,10 @@ const FormPage = () => {
           name="phone"
           rules={[{ validator: validatePhone }]}
         >
-          <Input placeholder="请输入手机号😃（支持13/14/15/16/17/18/19开头）" />
+          <Input
+            placeholder="请输入手机号😃（支持13/14/15/16/17/18/19开头）"
+            style={{ border: "1px solid green" }}
+          />
         </Form.Item>
 
         <Form.Item label="性别" name="gender" rules={[{ required: true }]}>
